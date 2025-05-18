c38ea084bb5224efc233e8bf44241a6674782b96
zeluism1 <89844092+zeluism1@users.noreply.github.com>
2025-05-18 12:32:39 +0100

    cleanup

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"node_modules/i18next-http-backend/esm/request.js -> cross-fetch\":{\"id\":\"node_modules/i18next-http-backend/esm/request.js -> cross-fetch\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_cross-fetch_dist_browser-ponyfill_js.js\"]}}"
